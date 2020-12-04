8e8ab2383496e98df33048ee2eeb9fb952c55de8
hollowly <2499538506@qq.com>
2020-12-04 12:28:30 +0800

    enter

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// dataV：插件注册和使用
+import dataV from '@jiaminghi/data-view'
+import { borderBox1 } from '@jiaminghi/data-view'
+
+Vue.use(dataV)
+Vue.use(borderBox1)
+
 
 Vue.config.productionTip = false
 
