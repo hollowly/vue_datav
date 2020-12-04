4b96651fc971aee46a138d6d0e76477cd66f737f
hollowly <2499538506@qq.com>
2020-12-04 12:59:19 +0800

    enter

--- src/components/btn.vue
+++ src/components/btn.vue
@@ -1,40 +1,38 @@
 <!--  -->
 <template>
-	<div>
-		<button class="btn">
-			<span>提交</span>
-			<svg width="0" height="0">
-				<defs>
-					<filter id="goo" x="-50%" width="200%" y="-50%" height="200%" color-interpolation-filters="sRGB">
-						<feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur" />
-						<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7" result="cm" />
-					</filter>
-					<filter id="goo-light" x="-50%" width="200%" y="-50%" height="200%" color-interpolation-filters="sRGB">
-						<feGaussianBlur in="SourceGraphic" stdDeviation="1.25" result="blur" />
-						<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7" result="cm" />
-					</filter>
-					<filter id="goo-big" x="-50%" width="200%" y="-50%" height="200%" color-interpolation-filters="sRGB">
-						<feGaussianBlur in="SourceGraphic" stdDeviation="7" result="blur" />
-						<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7" result="cm" />
-					</filter>
-				</defs>
-			</svg>
-			<svg preserveAspectRatio="none" viewBox="0 0 132 45">
-				<g clip-path="url(#clip)" filter="url(#goo-big)">
-					<circle class="top-left" cx="49.5" cy="-0.5" r="26.5" />
-					<circle class="middle-bottom" cx="70.5" cy="40.5" r="26.5" />
-					<circle class="top-right" cx="104" cy="6.5" r="27" />
-					<circle class="right-bottom" cx="123.5" cy="36.5" r="26.5" />
-					<circle class="left-bottom" cx="16.5" cy="28" r="30" />
-				</g>
-				<defs>
-					<clipPath id="clip">
-						<rect width="132" height="45" rx="7" />
-					</clipPath>
-				</defs>
-			</svg>
-		</button>
-	</div>
+<button class="btn">
+	<span>扩展</span>
+	<svg width="0" height="0">
+		<defs>
+			<filter id="goo" x="-50%" width="200%" y="-50%" height="200%" color-interpolation-filters="sRGB">
+				<feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur" />
+				<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7" result="cm" />
+			</filter>
+			<filter id="goo-light" x="-50%" width="200%" y="-50%" height="200%" color-interpolation-filters="sRGB">
+				<feGaussianBlur in="SourceGraphic" stdDeviation="1.25" result="blur" />
+				<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7" result="cm" />
+			</filter>
+			<filter id="goo-big" x="-50%" width="200%" y="-50%" height="200%" color-interpolation-filters="sRGB">
+				<feGaussianBlur in="SourceGraphic" stdDeviation="7" result="blur" />
+				<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7" result="cm" />
+			</filter>
+		</defs>
+	</svg>
+	<svg preserveAspectRatio="none" viewBox="0 0 132 45">
+		<g clip-path="url(#clip)" filter="url(#goo-big)">
+			<circle class="top-left" cx="49.5" cy="-0.5" r="26.5" />
+			<circle class="middle-bottom" cx="70.5" cy="40.5" r="26.5" />
+			<circle class="top-right" cx="104" cy="6.5" r="27" />
+			<circle class="right-bottom" cx="123.5" cy="36.5" r="26.5" />
+			<circle class="left-bottom" cx="16.5" cy="28" r="30" />
+		</g>
+		<defs>
+			<clipPath id="clip">
+				<rect width="132" height="45" rx="7" />
+			</clipPath>
+		</defs>
+	</svg>
+</button>
 </template>
 
 <script>
@@ -50,60 +48,58 @@ export default {
 </script>
 
 <style scoped>
-#btnSvg {
-	padding: none;
-	margin: none;
-	border: 1px solid  rgb(0,78,102);
-}
-
-.btn {
-  border: none;
-  position: relative;
-  background: black;
-  color: rgb(126,198,153);
-  outline: none;
-  cursor: pointer;
-  font-size: 14px;
-  line-height: 21px;
-  font-weight: 600;
-  display: block;
-  width: 132px;
-  border-radius: 7px;
-  padding: 12px 0;
-}
-.btn > span {
-  position: relative;
-  z-index: 1;
-}
-.btn > svg {
-  display: block;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-  fill: #275efe;
-}
-.btn > svg circle {
-  transition: 0.5s ease-out;
-}
-.btn > svg circle.top-left {
-  transform: translate(var(--spacing, -16px), var(--spacing, -40px));
-}
-.btn > svg circle.middle-bottom {
-  transform: translate(var(--spacing, 8px), var(--spacing, 40px));
-}
-.btn > svg circle.top-right {
-  transform: translate(var(--spacing, 40px), var(--spacing, -40px));
-}
-.btn > svg circle.right-bottom {
-  transform: translate(var(--spacing, 4px), var(--spacing, 40px));
-}
-.btn > svg circle.left-bottom {
-  transform: translate(var(--spacing, -40px), var(--spacing, 40px));
-}
-.btn:hover {
-  --spacing: 0;
-}
-
+	.btn {
+		border: none;
+		position: relative;
+		background: black;
+		color: white;
+		outline: none;
+		cursor: pointer;
+		font-size: 14px;
+		line-height: 21px;
+		font-weight: 600;
+		display: block;
+		width: 132px;
+		border-radius: 7px;
+		padding: 12px 0;
+		border:1px solid rgb(126,198,126);
+		box-sizing: border-box;
+	}
+	.btn:hover {
+		border:1px solid black;
+	}
+	.btn > span {
+		position: relative;
+		z-index: 1;
+	}
+	.btn > svg {
+		display: block;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		left: 0;
+		top: 0;
+		fill: rgb(126,198,126);
+	}
+	.btn > svg circle {
+		transition: 0.5s ease-out;
+	}
+	.btn > svg circle.top-left {
+		transform: translate(var(--spacing, -16px), var(--spacing, -40px));
+	}
+	.btn > svg circle.middle-bottom {
+		transform: translate(var(--spacing, 8px), var(--spacing, 40px));
+	}
+	.btn > svg circle.top-right {
+		transform: translate(var(--spacing, 40px), var(--spacing, -40px));
+	}
+	.btn > svg circle.right-bottom {
+		transform: translate(var(--spacing, 4px), var(--spacing, 40px));
+	}
+	.btn > svg circle.left-bottom {
+		transform: translate(var(--spacing, -40px), var(--spacing, 40px));
+	}
+	.btn:hover {
+		--spacing: 0;
+	}
 </style>
